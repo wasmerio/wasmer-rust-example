ee4ef2a43222b96b9aab4dad1ce67bfd0d2db8bd
Brandon Fish <brandon.j.fish@gmail.com>
2019-01-22 19:52:17 -0600

    Update rust wasm example

--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -1,13 +1,20 @@
-
-// Defines functions to import from "env" namespace
+// Define a function that is imported into the module.
+// By default, the "env" namespace is used.
 extern "C" {
     fn print_str(ptr: *const u8, len: usize);
 }
 
+// Define a string that is accessible within the wasm
+// linear memory.
+static HELLO: &'static str = "Hello, World!";
+
+// Export a function named "hello_wasm". This can be called
+// from the embedder!
 #[no_mangle]
-pub extern fn hello_wasm(){
-    let message = "Hello World";
+pub extern fn hello_wasm() {
+    // Call the function we just imported and pass in
+    // the offset of our string and its length as parameters.
     unsafe {
-      print_str(message.as_ptr(), message.len());
+      print_str(HELLO.as_ptr(), HELLO.len());
     }
 }
