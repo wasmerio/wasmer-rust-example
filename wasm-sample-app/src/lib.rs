975f1bd58e8d8e61de62c8a6fbb700a91c187990
Mark McCaskey <mark@wasmer.io>
2020-01-22 15:34:16 -0800

    Update to Wasmer 0.13.1 and add host fn Closures as an example

--- wasm-sample-app/src/lib.rs
+++ wasm-sample-app/src/lib.rs
@@ -1,10 +1,12 @@
-use std::str;
 use std::slice;
+use std::str;
 
 // Define a function that is imported into the module.
 // By default, the "env" namespace is used.
 extern "C" {
     fn print_str(ptr: *const u8, len: usize);
+    fn print_str2(ptr: *const u8, len: usize);
+    fn increment_shared();
 }
 
 // Define a string that is accessible within the wasm
@@ -14,21 +16,24 @@ static HELLO: &'static str = "Hello, World!";
 // Export a function named "hello_wasm". This can be called
 // from the embedder!
 #[no_mangle]
-pub extern fn hello_wasm() {
+pub extern "C" fn hello_wasm() {
     // Call the function we just imported and pass in
     // the offset of our string and its length as parameters.
     unsafe {
-      print_str(HELLO.as_ptr(), HELLO.len());
+        print_str(HELLO.as_ptr(), HELLO.len());
+        print_str2(HELLO.as_ptr(), HELLO.len());
+        increment_shared();
+        increment_shared();
+        print_str2(HELLO.as_ptr(), HELLO.len());
     }
 }
 
-
 #[no_mangle]
-pub extern fn hello_string_from_rust(ptr: i32, len: i32) {
+pub extern "C" fn hello_string_from_rust(ptr: i32, len: i32) {
     let slice = unsafe { slice::from_raw_parts(ptr as _, len as _) };
     let string_from_host = str::from_utf8(&slice).unwrap();
     let out_str = format!("Hello {}", string_from_host);
     unsafe {
-      print_str(out_str.as_ptr(), out_str.len());
+        print_str(out_str.as_ptr(), out_str.len());
     }
 }
